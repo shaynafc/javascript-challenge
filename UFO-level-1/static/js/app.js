6748104832b7d387d3040502bc331ff1f611045f
shaynafc <shayna.cher@gmail.com>
2021-01-04 15:38:44 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,54 @@
-// from data.js
+// Starter Code
 var tableData = data;
 
-// YOUR CODE HERE!
+
+// Viewing the available data fromt he data.js
+// console.log(tableData);
+
+
+// Creating References to tbody, input and button
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+
+// Inputing the data into the HTML
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
+
+
+button.on("click", () => {
+
+    d3.event.preventDefault();
+    
+
+    var inputDate = inputFieldDate.property("value").trim();
+     
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    
+
+    $tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+
+    if(response.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+
+    
+        else {
+            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+        }
+})
